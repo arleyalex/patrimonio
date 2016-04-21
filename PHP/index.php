8f3ce850a1f7231b55256bd0c12e935f19d3972a
JairoCarneiro <jairoccjunior@gmail.com>
2016-04-21 11:24:07 -0400

    updated index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,42 +1,35 @@
-<?php
-
-phpinfo();
-session_start();
-if (isset($_SESSION['usuario'])){
-	header('Location: menu1.html');
-}else{
-	
-    //SISTEMA PATRIMÔNIAL
-    $conexao = pg_connect("host=192.168.40.188 dbname=db_patrimonio port=5432 user=postgres password=123456") or die("ERRRO");
-    
-    if (!$conexao) {
-            echo "Não foi possível estabelecer uma conexão com o banco de dados.";
-            print  'Não foi possível estabelecer uma conexão com o banco de dados22.';
+    <?php
+    session_start();
+    if (isset($_SESSION['login'])) {
+        header('Location: menu1.html');
     } else {
-           echo "OK.";
-            print  'OK22.';
-
-    }
-}
-    if (isset($_POST['login']) && isset($_POST['senha'])){
+     
+        $BANCO = ("host=localhost port=5432 dbname=db_patrimonio port=5432 user=postgres password=123456");
+        $CONEXAO = pg_conect($BANCO);
+        if (!$CONEXAO) {
+            echo 'SEM CONEXÃO COM O BANCO DE DADOS';
+            exit();
+        } else {
+             if (isset($_POST['login']) && isset($_POST['senha'])){
 			$login = $_POST['login'];
 			$senha= $_POST['senha'];
-			$sql = "SELECT login, nome FROM usuario WHERE login = '" . $login . "' " . " AND senha = '" . md5($senha) . "'";
-			$result = pg_query($conexao, $sql);
-			if (pg_num_rows($result)){ 
-				$linha = pg_fetch_array($result);
+			$SQL = "SELECT login, nome FROM usuario WHERE login = '" . $login . "' " . " AND senha = '" . md5($senha) . "'";
+			$RESULTADO = pg_query($CONEXAO, $SQL);
+			if (pg_num_rows($RESULTADO)){ 
+				$LINHA = pg_fetch_array($RESULTADO);
 				$_SESSION['usuario'] = $linha['login'];
 				$_SESSION['nome_usuario'] = $linha['nome'];
 				header('Location: menu1.html');
 			}else{
-				echo "<p>Usuário inexistente!!!</p>";
-			}
-   
+				echo "<p>USUARIO NÃO CADASTRADO.</p>";
+                                exit();
+                                
+                        }
+                        
+            
+        }
+    }
+    }
+    pg_close($CONEXAO);
+    ?>
      
-    
-pg_close($conexao);
-    //print "Conexão OK!";
-}
-?>
- 
- 
