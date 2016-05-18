c6d5b7335d38727119769bac66294a04821d09e6
rvazdias <rvazdias2014@gmail.com>
2016-05-18 19:47:34 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,33 @@
+<?php
+
+    $METODO = getenv("REQUEST_METHOD");
+    $BANCO = ("host=localhost port=5432 dbname=cygni port=5432 user=postgres password=root");
+    $CONEXAO = pg_connect($BANCO);
+    if ($METODO == "GET"){
+    echo "DADOS INVALIDOS";
+    }
+    if (!$CONEXAO) {
+    echo 'SEM CONEXÃO COM O BANCO DE DADOS';
+   }
+    else{
+        $login = $_POST['login'];
+        $senha = $_POST['senha'];
+        $SQL = "SELECT login, nome FROM usuario WHERE login = '" . $login . "' " . " AND senha = '" . md5($senha) . "'";
+        $RESULTADO = pg_query($CONEXAO, $SQL);
+        if (pg_num_rows($RESULTADO)) {
+            $LINHA = pg_fetch_array($RESULTADO);
+            $_SESSION['usuario'] = $LINHA['login'];
+            $_SESSION['nome_usuario'] = $LINHA['nome'];
+            header('Location: menu1.html');
+        } else {
+            echo "<p>USUARIO NÃO ACEITO.</p>";
+            exit();
+        }
+
+}
+    pg_close($CONEXAO);
+?>
+    <html>
+    <title> SISTEMA PATRIMONIAL </title>
+    </html>
+~
