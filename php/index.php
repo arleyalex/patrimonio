dcf2202ffab3047d48d9eaad55a5feb5b3b48191
André de Oliveira Silva <andreoliversilva@gmail.com>
2016-05-18 22:52:32 -0300

    Atualização index

--- php/index.php
+++ php/index.php
@@ -1,8 +1,6 @@
 <?php
 
-    $METODO = getenv("REQUEST_METHOD");
-    $BANCO = ("host=localhost port=5432 dbname=cygni port=5432 user=postgres password=root");
-    $CONEXAO = pg_connect($BANCO);
+    include "conexao.php";
     if ($METODO == "GET"){
     echo "DADOS INVALIDOS";
     }
