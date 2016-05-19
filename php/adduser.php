1d2423c26e14035c072d28cc3ccc36eb87bdc757
André de Oliveira Silva <andreoliversilva@gmail.com>
2016-05-18 22:52:04 -0300

    Atualização do código

--- php/adduser.php
+++ php/adduser.php
@@ -1,21 +1,19 @@
     <?php
-    $url= "host=192.168.44.144 port=5432 dbname=db_patrimonio user=postgres password=123456";
-     
-    $conexao = pg_connect($url) or die ("Não foi possivel conectar ao servidor PostGreSQL"); 
-     
+    include "conexao.php";
+	    
     $login = $_POST['login'];
 	$nome = $_POST['nome'];
 	$senha = $_POST['senha'];
 	$perfil = $_POST['perfil'];
 	
-	$sql = "INSERT INTO usuario (login, nome, senha, nivel) VALUES ('$login','$nome',MD5('$senha'),'$perfil')";
-	$result = pg_query ($conexao, $sql);
-	if ($result != FALSE){
+	$SQL = "INSERT INTO usuario (login, nome, senha, nivel) VALUES ('$login','$nome',MD5('$senha'),'$perfil')";
+	$RESULTADO = pg_query ($CONEXAO, $SQL);
+	if ($RESULTADO != FALSE){
 		echo "<h2>Dados inseridos com sucesso!</h2><br>";
 	}
 	else {
 		echo "<h2>Falha no cadastro do usuário!</h2><br>";
 	}	
-	pg_close($conexao);
+	pg_close($CONEXAO);
     ?>
 
