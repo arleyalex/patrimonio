3b64469f040eebb1969cbac0eda81831d0403c04
jairo <jairo@localhost>
2016-05-24 12:45:04 -0400

    file add

--- php/conexao.php
+++ php/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-	$METODO = getenv("REQUEST_METHOD");
+    $METODO = getenv("REQUEST_METHOD");
     $BANCO = ("host=192.168.45.188 port=5432 dbname=cygni port=5432 user=postgres password=root");
     $CONEXAO = pg_connect($BANCO);
 	
--- php/index.php
+++ php/index.php
@@ -1,31 +1,37 @@
 <?php
-
-    include "conexao.php";
-    if ($METODO == "GET"){
+include "conexao.php";
+if ($METODO == "GET") {
     echo "DADOS INVALIDOS";
-    }
-    if (!$CONEXAO) {
-    echo 'SEM CONEX√ÉO COM O BANCO DE DADOS';
-   }
-    else{
-        $login = $_POST['login'];
-        $senha = $_POST['senha'];
-        $SQL = "SELECT login, nome FROM usuario WHERE login = '" . $login . "' " . " AND senha = '" . md5($senha) . "'";
-        $RESULTADO = pg_query($CONEXAO, $SQL);
-        if (pg_num_rows($RESULTADO)) {
-            $LINHA = pg_fetch_array($RESULTADO);
-            $_SESSION['usuario'] = $LINHA['login'];
-            $_SESSION['nome_usuario'] = $LINHA['nome'];
-            header('Location: menu1.html');
-        } else {
-            echo "<p>USUARIO N√ÉO ACEITO.</p>";
-            exit();
-        }
-
 }
-    pg_close($CONEXAO);
+if (!$CONEXAO) {
+    echo 'SEM CONEX√O COM O BANCO DE DADOS';
+} else {
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+    $NIVEL = $_POST['nivel'];
+    $SQL = "SELECT login, nome, nivel FROM usuario WHERE login = '" . $login . "' " . " AND senha = '" . md5($senha) . "'";
+    $RESULTADO = pg_query($CONEXAO, $SQL);
+    if (pg_num_rows($RESULTADO)) {
+        $LINHA = pg_fetch_array($RESULTADO);
+        switch ($NIVEL) {
+            case "G":
+                $_SESSION['usuario'] = $LINHA['login'];
+                $_SESSION['nome_usuario'] = $LINHA['nome'];
+                header('Location: consulta2.html');
+
+            case "F":
+                $_SESSION['usuario'] = $LINHA['login'];
+                $_SESSION['nome_usuario'] = $LINHA['nome'];
+                header('Location: consulta1.html');
+        }
+    } else {
+        echo "<p>USUARIO N√O ACEITO.</p>";
+        exit();
+    }
+}
+pg_close($CONEXAO);
 ?>
-    <html>
+<html>
     <title> SISTEMA PATRIMONIAL </title>
-    </html>
+</html>
 ~
