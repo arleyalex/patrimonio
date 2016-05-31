c4c066c4b91db77d4257b54d433230660189835b
jairo <jairo@localhost>
2016-05-30 20:23:40 -0400

    dsfSDF

--- php/consultapt.php
+++ php/consultapt.php
@@ -1,18 +1,33 @@
 <?php
 
 session_start();
+ include "conexao.php";
+
+?>
+
     <html>
         <head>
-        <title>CADASTRAMENTO DE PATRIMONIO</title>
+        <title>LISTAGEM DE PATRIMONIO</title>
         <meta content = "text/html" charset = "utf-8"/>
-        <form name = "frmI" id = "frmI" action = "" method = "POST" onSubmit = "javascript: return verificar();" >
+        <form name = "frmL" id = "frmL" action = "" method = "POST" > <!-- onSubmit = "javascript: return verificar();"-->
         </head>
 
                 <body>
                         <fieldset>
-                            <legend>Entre com os dados</legend>
+                            <legend>BENS PATRIMONIAIS</legend>
 
-                                    <p></p>
+                                    <?php
+                                    $SQL = "SELECT descricao FROM bempatrimonial";
+                                    $RESULTADO = pg_query($SQL);
+                                    if (pg_num_rows($RESULTADO)) {
+                                        while ($LINHA = pg_fetch_array($RESULTADO)){
+                                                echo "<p>".$LINHA["descricao"]."</p";
+                                        }
+                                    }else{
+                                        echo "Não existem bens patrimoniais cadastrados.";
+                                    }
+                                    ?>
+                            <p></p>
                                     DADO1:- <input type = "number" name = "matricula" id = "matricula" value = "" maxlength = "12"><br>
                                     DADO2:- <input type = "text" name = "nome" id = "nome" value = "" maxlength = "50"><br>
                                     <input type = "submit" name = "onload" id = "onload" value = "Cadastrar" />
@@ -22,19 +37,21 @@ session_start();
                 </body>
     </html>
         
-        
-        $SQL = "INSERT ...."
-        $RESULTADO = pg_query($SQL);
-        if (pg_num_rows($RESULTADO)) {
-        $LINHA = pg_fetch_array($RESULTADO);
-        echo "DADOS INSERIDOS COM SUCESSO."
+<?php        
 
-        
-        
-        
-        
+
        pg_close($CONEXAO); 
         
         
 ?>
 
+
+    
+    CREATE TABLE bemPatrimonial (
+	numero
+        nrNotaFiscal INTEGER NOT NULL,
+	fornecedor VARCHAR(60) NOT NULL,
+	situacao CHAR(1) NOT NULL,
+	codCat
+	numSala
+	
