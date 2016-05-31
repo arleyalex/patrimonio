86ef05e78aedc56490eedcc47b565c97a23e23f5
André de Oliveira Silva <andreoliversilva@gmail.com>
2016-05-30 21:28:56 -0300

    Correção do código
    Foi incluindo o Array para armazenar a consulta.

--- php/index.php
+++ php/index.php
@@ -4,34 +4,28 @@ if ($METODO == "GET") {
     echo "DADOS INVALIDOS";
 }
 if (!$CONEXAO) {
-    echo 'SEM CONEX√O COM O BANCO DE DADOS';
+    echo 'SEM CONEX√ÉO COM O BANCO DE DADOS';
 } else {
     $login = $_POST['login'];
     $senha = $_POST['senha'];
-    $NIVEL = $_POST['nivel'];
     $SQL = "SELECT login, nome, nivel FROM usuario WHERE login = '" . $login . "' " . " AND senha = '" . md5($senha) . "'";
     $RESULTADO = pg_query($CONEXAO, $SQL);
-    if (pg_num_rows($RESULTADO)) {
-        $LINHA = pg_fetch_array($RESULTADO);
-        switch ($NIVEL) {
-            case "G":
-                $_SESSION['usuario'] = $LINHA['login'];
-                $_SESSION['nome_usuario'] = $LINHA['nome'];
-                header('Location: consulta2.html');
+	$DADOS = pg_fetch_row ($RESULTADO);
+	$NIVEL = $DADOS[2];
+	switch ($NIVEL) {
+		case "G":
+			//$_SESSION['usuario'] = $LINHA['login'];
+            //$_SESSION['nome_usuario'] = $LINHA['nome'];
+            header('Location: ../consulta2.html');
 
-            case "F":
-                $_SESSION['usuario'] = $LINHA['login'];
-                $_SESSION['nome_usuario'] = $LINHA['nome'];
-                header('Location: consulta1.html');
+        case "F":
+            //$_SESSION['usuario'] = $LINHA['login'];
+            //$_SESSION['nome_usuario'] = $LINHA['nome'];
+            header('Location: ../consulta1.html');
         }
-    } else {
-        echo "<p>USUARIO N√O ACEITO.</p>";
-        exit();
     }
-}
 pg_close($CONEXAO);
 ?>
 <html>
     <title> SISTEMA PATRIMONIAL </title>
-</html>
-~
+</html>	
