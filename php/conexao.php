a9f1c4e9d70ee03385f9e47677377d01db0f7643
André de Oliveira Silva <andreoliversilva@gmail.com>
2016-06-02 18:00:19 -0300

    Atualização código
    Alterado campo host para localhost. Lembrar de alterar arquivo ao baixar aquivo para teste.

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php
 
     $METODO = getenv("REQUEST_METHOD");
-    $BANCO = ("host=192.168.45.188 port=5432 dbname=cygni port=5432 user=postgres password=root");
+    $BANCO = ("host=localhost port=5432 dbname=cygni port=5432 user=postgres password=root");
     $CONEXAO = pg_connect($BANCO);
 	
 ?>
