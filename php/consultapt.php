329c7c4a844ebf545e23a8be93bf862111f1d4af
André de Oliveira Silva <andreoliversilva@gmail.com>
2016-06-03 00:28:05 -0300

    Atualização de página
    Inclusão de tamplates
    Ajuste de código

--- php/consultapt.php
+++ php/consultapt.php
@@ -1,57 +1,78 @@
 <?php
+include "conexao.php";
 
 session_start();
- include "conexao.php";
 
 ?>
 
-    <html>
-        <head>
-        <title>LISTAGEM DE PATRIMONIO</title>
-        <meta content = "text/html" charset = "utf-8"/>
-        <form name = "frmL" id = "frmL" action = "" method = "POST" > <!-- onSubmit = "javascript: return verificar();"-->
-        </head>
-
-                <body>
-                        <fieldset>
-                            <legend>BENS PATRIMONIAIS</legend>
-
-                                    <?php
-                                    $SQL = "SELECT descricao FROM bempatrimonial";
-                                    $RESULTADO = pg_query($SQL);
-                                    if (pg_num_rows($RESULTADO)) {
-                                        while ($LINHA = pg_fetch_array($RESULTADO)){
-                                                echo "<p>".$LINHA["descricao"]."</p";
-                                        }
-                                    }else{
-                                        echo "N�o existem bens patrimoniais cadastrados.";
-                                    }
-                                    ?>
-                            <p></p>
-                                    DADO1:- <input type = "number" name = "matricula" id = "matricula" value = "" maxlength = "12"><br>
-                                    DADO2:- <input type = "text" name = "nome" id = "nome" value = "" maxlength = "50"><br>
-                                    <input type = "submit" name = "onload" id = "onload" value = "Cadastrar" />
-                                    <input type = "reset" name = "btnLimpar" id = "btnLimpar" value = "Limpar"/><br>
-                                    <a href = "consulta2.html">Voltar a pagina</a> <br>
-                              </fieldset>
-                </body>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../bootstrap/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <link href="../bootstrap/css/dashboard.css" rel="stylesheet">
+        <link href="../bootstrap/css/carousel.css" rel="stylesheet">
+        <link href="../bootstrap/css/estilomenu.css" rel="stylesheet">
+		<title>Sistema Patrimonial</title>
+    </head>
+    
+    <body>
+	<div class="container">
+    	<nav class="navbar navbar-inverse navbar-fixed-top">
+      		<div class="container-fluid">
+       			 <div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+          			</button>
+          			<a class="navbar-brand" href="#">Sistema Patrimonial - Consultar Item</a>
+       			 </div>
+        		<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+                    <li><a href="php/cadastrarbem.php">Cadastrar Itens</a></li>
+                    <li><a href="php/excluir1.php">Excluir</a></li>
+                    <li><a href="php/sair.php">Sair</a></li>
+					</ul>
+        		</div>
+    		  </div>
+			</nav>
+			<br>
+			<br>
+			<br>
+			
+         <fieldset>
+                <legend>Digite o número do Patrimônio</legend>
+					<p></p>
+					    Nº Matrícula: <input type = "number" class="form-group" name = "matricula" id = "matricula" value = "" maxlength = "12"><br>
+						<p>
+						<p>
+                        <input type = "submit" class="form-group" name = "onload" id = "onload" value = "Consultar" />
+                        <input type = "reset" class="form-group" name = "btnLimpar" id = "btnLimpar" value = "Limpar"/><br>
+						<p>
+						<p>
+						<a href = "../consultar.html">Voltar a pagina Inicial</a> <br>
+		</fieldset>
+	 </div>                         
+     </body>
     </html>
         
-<?php        
+  <?php      
+    //    $SQL = "SELECT "
+    //    $RESULTADO = pg_query($SQL);
+    //    if (pg_num_rows($RESULTADO)) {
+    //    $LINHA = pg_fetch_array($RESULTADO);
+    //    echo "DADOS INSERIDOS COM SUCESSO."
 
-
-       pg_close($CONEXAO); 
+        
+        
+        
+        
+      // pg_close($CONEXAO); 
         
         
 ?>
 
-
-    
-    CREATE TABLE bemPatrimonial (
-	numero
-        nrNotaFiscal INTEGER NOT NULL,
-	fornecedor VARCHAR(60) NOT NULL,
-	situacao CHAR(1) NOT NULL,
-	codCat
-	numSala
-	
